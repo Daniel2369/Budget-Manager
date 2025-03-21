c343c4fdf015e55b2c99ecbf04531e6dd1aa094b
Daniel Berliant <berliantdaniel@gmail.com>
2025-03-21 09:43:13 +0200

    Added functions.py module file

---
+++ functions.py
@@ -0,0 +1,31 @@
+# Functions.py file module
+
+# Income function - args: incomes, expenses, return: tuple(incomes, expenses)
+#def add_income(incomes: list, expenses: list) -> tuple:
+    #return incomes, expenses
+
+
+
+
+
+#def add_expense(incomes: list, expenses: list) -> tuple:
+    #return incomes, expenses
+
+
+
+
+
+#def show_balance(incomes: list, expenses: list) -> tuple:
+    #return incomes, expenses
+
+
+
+
+
+
+#def show_transaction_history(incomes: list, expenses: list) -> tuple:
+    #return incomes, expenses
+
+
+
+
